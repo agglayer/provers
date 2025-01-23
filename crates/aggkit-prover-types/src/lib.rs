376ac0f30f81d95b6be1e01818d61f37fbb36e75
Arpit Temani <temaniarpit27@gmail.com>
2025-01-23 18:50:14 +0400

    feat: protobuf def for auth proof service

---
+++ crates/aggkit-prover-types/src/lib.rs
@@ -0,0 +1,20 @@
+use bincode::{
+    config::{BigEndian, FixintEncoding, WithOtherEndian, WithOtherIntEncoding},
+    DefaultOptions, Options,
+};
+pub const FILE_DESCRIPTOR_SET: &[u8] = include_bytes!("generated/aggkit.prover.bin");
+
+#[path = "generated/aggkit.prover.v1.rs"]
+#[rustfmt::skip]
+#[allow(warnings)]
+pub mod v1;
+
+pub fn default_bincode_options(
+) -> WithOtherIntEncoding<WithOtherEndian<DefaultOptions, BigEndian>, FixintEncoding> {
+    DefaultOptions::new()
+        .with_big_endian()
+        .with_fixint_encoding()
+}
+
+pub mod error;
+pub use error::Error;
