51113bcdb682b23da5aeabee91650d549818c837
Monir Hadji <hadji.szs@gmail.com>
2025-02-19 18:55:23 +0100

    feat: add inserted ger verif

--- crates/aggchain-proof-core/src/error.rs
+++ crates/aggchain-proof-core/src/error.rs
@@ -1,5 +1,11 @@
 use serde::{Deserialize, Serialize};
 
+use crate::bridge::BridgeConstraintsError;
+
 /// Represents all the aggchain proof errors.
 #[derive(Clone, thiserror::Error, Debug, Serialize, Deserialize, PartialEq, Eq)]
-pub enum ProofError {}
+pub enum ProofError {
+    /// Error on the bridge constraints.
+    #[error("Failure on the bridge constraints.")]
+    BridgeConstraintsFailure(#[from] BridgeConstraintsError),
+}
