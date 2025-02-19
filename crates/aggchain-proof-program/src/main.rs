62f7ce2817dd5ce5609114bfdbc532a36996ac3a
Monir Hadji <hadji.szs@gmail.com>
2025-02-19 14:23:37 +0100

    feat: add let utils

--- crates/aggchain-proof-program/src/main.rs
+++ crates/aggchain-proof-program/src/main.rs
@@ -2,18 +2,11 @@
 sp1_zkvm::entrypoint!(main);
 
 use aggchain_proof_core::proof::AggchainProofWitness;
-use bincode::Options;
 
 pub fn main() {
     let mut aggchain_witness: AggchainProofWitness = sp1_zkvm::io::read::<AggchainProofWitness>();
 
-    let outputs = aggchain_witness.generate_aggchain_proof().unwrap();
+    let aggchain_proof_public_values = aggchain_witness.generate_aggchain_proof().unwrap();
 
-    let aggchain_proof_public_inputs = bincode::DefaultOptions::new()
-        .with_big_endian()
-        .with_fixint_encoding()
-        .serialize(&outputs)
-        .unwrap();
-
-    sp1_zkvm::io::commit_slice(&aggchain_proof_public_inputs);
+    sp1_zkvm::io::commit(&aggchain_proof_public_values);
 }
