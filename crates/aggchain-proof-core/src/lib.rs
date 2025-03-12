472bd87cc4125d647c50bd151e7cdc251c1f2c37
Monir H. <hadji.szs@gmail.com>
2025-03-12 22:33:52 +0400

    feat: add remaining constraints in aggchain proof (#84)
    * feat: add hash chain verif on claims
    
    * feat: use hardcoded aggregation vkey
    
    * feat: verif l1 head
    
    * feat: use non-hashed indices for hashchain
    
    * feat: refine hashchain errors for ger vs claims
    
    * feat: add optimistic mode and align pv
    
    * chore: revert wip hashchain global index
    
    * chore: misc
    
    * feat: base for remaining hash chain
    
    * feat: add new hash chains
    
    * fix: inverted values
    
    * chore: apply review
    
    - make AGGREGATION_VKEY_HASH public
    - renaming
    - cloned to copied
    
    ---------
    
    Co-authored-by: invocamanman <jesus.ligero@yahoo.es>

--- crates/aggchain-proof-core/src/lib.rs
+++ crates/aggchain-proof-core/src/lib.rs
@@ -6,4 +6,6 @@ mod local_exit_tree;
 pub mod proof;
 pub mod vkey_hash;
 
+pub use bridge::inserted_ger::L1InfoTreeLeaf;
+pub use full_execution_proof::AGGREGATION_VKEY_HASH;
 pub use keccak::digest::Digest;
