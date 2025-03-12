8e45f67fe89e0a693bf5b026e8c960da0495bb52
Monir Hadji <hadji.szs@gmail.com>
2025-03-12 16:24:04 +0100

    feat: generate sp1 stdin for aggchain proof

--- crates/aggchain-proof-builder/src/lib.rs
+++ crates/aggchain-proof-builder/src/lib.rs
@@ -5,15 +5,23 @@ use std::sync::Arc;
 use std::task::{Context, Poll};
 
 use aggchain_proof_contracts::contracts::{
-    L1RollupConfigHashFetcher, L2LocalExitRootFetcher, L2OutputAtBlockFetcher,
+    L1RollupConfigHashFetcher, L2EVMStateSketchesFetched, L2LocalExitRootFetcher,
+    L2OutputAtBlockFetcher,
 };
 use aggchain_proof_contracts::AggchainContractsClient;
+use aggchain_proof_core::full_execution_proof::FepPublicValues;
+use aggchain_proof_core::local_exit_tree::hasher::Keccak256Hasher;
+use aggchain_proof_core::local_exit_tree::proof::LETMerkleProof;
 use aggchain_proof_core::proof::AggchainProofWitness;
+use aggchain_proof_core::{BridgeWitness, InsertedGER, L1InfoTreeLeaf};
 use aggchain_proof_types::{AggchainProofInputs, Digest};
+use alloy::eips::BlockNumberOrTag;
+use alloy_primitives::Address;
 pub use error::Error;
 use futures::{future::BoxFuture, FutureExt};
 use prover_executor::Executor;
-use sp1_sdk::{SP1Proof, SP1ProofWithPublicValues, SP1VerifyingKey};
+use sp1_sdk::network::B256;
+use sp1_sdk::{SP1Proof, SP1ProofWithPublicValues, SP1Stdin, SP1VerifyingKey};
 use tower::buffer::Buffer;
 use tower::util::BoxService;
 use tower::ServiceExt as _;
@@ -32,7 +40,7 @@ pub(crate) type ProverService = Buffer<
 /// All the data `aggchain-proof-builder` needs for the agghchain
 /// proof generation. Collected from various sources.
 pub struct AggchainProverInputs {
-    pub proof_witness: AggchainProofWitness,
+    pub sp1_stdin: SP1Stdin,
     pub start_block: u64,
     pub end_block: u64,
 }
@@ -78,6 +86,14 @@ pub struct AggchainProofBuilder<ContractsClient> {
     aggchain_proof_vkey: SP1VerifyingKey,
 }
 
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum WitnessGeneration {
+    #[error("Invalid inserted GER.")]
+    InvalidInsertedGer,
+    #[error("Cannot interpret the aggregation proof as 'compressed' type.")]
+    WrongAggregationProofType,
+}
+
 impl<ContractsClient> AggchainProofBuilder<ContractsClient> {
     pub async fn new(
         config: &AggchainProofBuilderConfig,
@@ -107,37 +123,150 @@ impl<ContractsClient> AggchainProofBuilder<ContractsClient> {
     pub(crate) async fn retrieve_chain_data(
         contracts_client: Arc<ContractsClient>,
         request: AggchainProofBuilderRequest,
+        origin_network: u32,
     ) -> Result<AggchainProverInputs, Error>
     where
-        ContractsClient:
-            L2LocalExitRootFetcher + L2OutputAtBlockFetcher + L1RollupConfigHashFetcher,
+        ContractsClient: L2LocalExitRootFetcher
+            + L2OutputAtBlockFetcher
+            + L2EVMStateSketchesFetched
+            + L1RollupConfigHashFetcher,
     {
-        let _prev_local_exit_root = contracts_client
+        // Fetch from RPCs
+        let prev_local_exit_root = contracts_client
             .get_l2_local_exit_root(request.aggchain_proof_inputs.start_block - 1)
             .await
             .map_err(Error::L2ChainDataRetrievalError)?;
 
-        let _new_local_exit_root = contracts_client
+        let new_local_exit_root = contracts_client
             .get_l2_local_exit_root(request.end_block)
             .await
             .map_err(Error::L2ChainDataRetrievalError)?;
 
-        let _l2_pre_root_output_at_block = contracts_client
+        let l2_pre_root_output_at_block = contracts_client
             .get_l2_output_at_block(request.aggchain_proof_inputs.start_block - 1)
             .await
             .map_err(Error::L2ChainDataRetrievalError)?;
 
-        let _claim_root_output_at_block = contracts_client
+        let claim_root_output_at_block = contracts_client
             .get_l2_output_at_block(request.end_block)
             .await
             .map_err(Error::L2ChainDataRetrievalError)?;
 
-        let _rollup_config_hash = contracts_client
+        let rollup_config_hash = contracts_client
             .get_rollup_config_hash()
             .await
             .map_err(Error::L1ChainDataRetrievalError)?;
 
-        todo!()
+        let prev_l2_block_sketch = contracts_client
+            .get_prev_l2_block_sketch(BlockNumberOrTag::Number(
+                request.aggchain_proof_inputs.start_block,
+            ))
+            .await
+            .map_err(Error::L2ChainDataRetrievalError)?;
+
+        let new_l2_block_sketch = contracts_client
+            .get_new_l2_block_sketch(BlockNumberOrTag::Number(request.end_block))
+            .await
+            .map_err(Error::L2ChainDataRetrievalError)?;
+
+        // From the request
+        let inserted_gers: Vec<InsertedGER> = request
+            .aggchain_proof_inputs
+            .ger_leaves
+            .values()
+            .map(|claim_data| {
+                Ok(InsertedGER {
+                    proof: claim_data.inclusion_proof.clone().try_into().map_err(|_| {
+                        Error::ProverWitnessGenerationError(WitnessGeneration::InvalidInsertedGer)
+                    })?,
+                    l1_info_tree_leaf: claim_data.l1_leaf.clone().into(),
+                    l1_info_tree_index: claim_data.l1_leaf.l1_info_tree_index,
+                })
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+
+        let inserted_gers_hash_chain = inserted_gers
+            .iter()
+            .map(|inserted_ger| inserted_ger.ger())
+            .collect();
+
+        let global_indices: Vec<B256> = request
+            .aggchain_proof_inputs
+            .imported_bridge_exits
+            .iter()
+            .map(|ib| (&ib.global_index).into())
+            .collect();
+
+        let l1_info_tree_leaf: (u32, L1InfoTreeLeaf) = (
+            request
+                .aggchain_proof_inputs
+                .l1_info_tree_leaf
+                .l1_info_tree_index,
+            request.aggchain_proof_inputs.l1_info_tree_leaf.into(),
+        );
+
+        // Considered empty for now
+        let (removed_gers_hash_chain, global_indices_unset) = (vec![], vec![]);
+
+        let trusted_sequencer = Address::default(); // TODO: from config or l1
+
+        let fep = FepPublicValues {
+            l1_head: l1_info_tree_leaf.1.block_hash,
+            claim_block_num: request.end_block as u32,
+            rollup_config_hash,
+            prev_state_root: l2_pre_root_output_at_block.state_root,
+            prev_withdrawal_storage_root: l2_pre_root_output_at_block.withdrawal_storage_root,
+            prev_block_hash: l2_pre_root_output_at_block.latest_block_hash,
+            new_state_root: claim_root_output_at_block.state_root,
+            new_withdrawal_storage_root: claim_root_output_at_block.withdrawal_storage_root,
+            new_block_hash: claim_root_output_at_block.latest_block_hash,
+            trusted_sequencer,
+            signature_optimistic_mode: None, // unsupported for now
+        };
+
+        let prover_witness = AggchainProofWitness {
+            prev_local_exit_root,
+            new_local_exit_root,
+            l1_info_root: request.aggchain_proof_inputs.l1_info_tree_root_hash,
+            origin_network,
+            fep,
+            l1_info_tree_leaf,
+            l1_head_inclusion_proof: LETMerkleProof::<Keccak256Hasher> {
+                siblings: request
+                    .aggchain_proof_inputs
+                    .l1_info_tree_merkle_proof
+                    .clone(),
+            },
+            global_indices: global_indices.clone(),
+            bridge_witness: BridgeWitness {
+                inserted_gers,
+                global_indices_claimed: global_indices,
+                global_indices_unset,
+                raw_inserted_gers: inserted_gers_hash_chain,
+                removed_gers: removed_gers_hash_chain,
+                prev_l2_block_sketch,
+                new_l2_block_sketch,
+            },
+        };
+
+        let SP1Proof::Compressed(aggregation_proof) = request.aggregation_proof.proof else {
+            return Err(Error::ProverWitnessGenerationError(
+                WitnessGeneration::WrongAggregationProofType,
+            ));
+        };
+        let aggregation_vkey = aggregation_proof.vk.clone();
+        let sp1_stdin = {
+            let mut stdin = SP1Stdin::new();
+            stdin.write(&prover_witness);
+            stdin.write_proof(*aggregation_proof, aggregation_vkey);
+            stdin
+        };
+
+        Ok(AggchainProverInputs {
+            start_block: request.aggchain_proof_inputs.start_block,
+            end_block: request.end_block,
+            sp1_stdin,
+        })
     }
 
     /// Generate aggchain proof
@@ -173,10 +302,12 @@ where
     fn call(&mut self, req: AggchainProofBuilderRequest) -> Self::Future {
         let contracts_client = self.contracts_client.clone();
         let prover = self.prover.clone();
+        let origin_network = self.network_id;
         async move {
             // Retrieve all the necessary public inputs. Combine with
             // the data provided by the agg-sender in the request.
-            let aggchain_prover_inputs = Self::retrieve_chain_data(contracts_client, req).await?;
+            let aggchain_prover_inputs =
+                Self::retrieve_chain_data(contracts_client, req, origin_network).await?;
 
             // Generate aggchain proof.
             Self::generate_aggchain_proof(prover, aggchain_prover_inputs).await
--- crates/aggchain-proof-types/src/proof.rs
+++ crates/aggchain-proof-types/src/proof.rs
@@ -1,6 +1,10 @@
 use std::collections::HashMap;
 
+use aggchain_proof_core::keccak::keccak256_combine;
+use aggchain_proof_core::local_exit_tree::hasher::Keccak256Hasher;
+use aggchain_proof_core::local_exit_tree::proof::LETMerkleProof;
 use alloy_primitives::Address;
+use alloy_primitives::FixedBytes;
 use serde::{Deserialize, Serialize};
 
 use crate::Digest;
@@ -9,6 +13,28 @@ use crate::Digest;
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct InclusionProof {
     pub siblings: Vec<Digest>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, thiserror::Error)]
+#[error("Invalid inclusion proof length. got: {got}, expected: {expected}")]
+pub struct InvalidInclusionProofLength {
+    got: usize,
+    expected: usize,
+}
+
+impl TryFrom<InclusionProof> for LETMerkleProof<Keccak256Hasher> {
+    type Error = InvalidInclusionProofLength;
+
+    fn try_from(value: InclusionProof) -> Result<Self, Self::Error> {
+        Ok(Self {
+            siblings: *value.siblings.first_chunk::<{ Self::TREE_DEPTH }>().ok_or(
+                InvalidInclusionProofLength {
+                    got: value.siblings.len(),
+                    expected: Self::TREE_DEPTH,
+                },
+            )?,
+        })
+    }
 }
 
 /// Claim from L1, used to prove the inclusion of the L1 info tree leaf.
@@ -28,6 +54,16 @@ pub struct L1InfoTreeLeaf {
     pub rollup_exit_root: Digest,
     pub mainnet_exit_root: Digest,
     pub inner_leaf: L1InfoTreeLeafInner,
+}
+
+impl From<L1InfoTreeLeaf> for aggchain_proof_core::L1InfoTreeLeaf {
+    fn from(value: L1InfoTreeLeaf) -> Self {
+        Self {
+            global_exit_root: keccak256_combine([value.mainnet_exit_root, value.rollup_exit_root]),
+            block_hash: value.inner_leaf.block_hash,
+            timestamp: value.inner_leaf.timestamp,
+        }
+    }
 }
 
 /// Represents the inner part of the leaf in the L1InfoTree.
@@ -51,6 +87,24 @@ pub struct GlobalIndex {
     pub mainnet_flag: bool,
     pub rollup_index: u32,
     pub leaf_index: u32,
+}
+
+impl From<&GlobalIndex> for FixedBytes<32> {
+    fn from(value: &GlobalIndex) -> Self {
+        let mut bytes = [0u8; 32];
+
+        let leaf_bytes = value.leaf_index.to_le_bytes();
+        bytes[0..4].copy_from_slice(&leaf_bytes);
+
+        let rollup_bytes = value.rollup_index.to_le_bytes();
+        bytes[4..8].copy_from_slice(&rollup_bytes);
+
+        if value.mainnet_flag {
+            bytes[8] |= 0x01;
+        }
+
+        bytes.into()
+    }
 }
 
 /// Encapsulates the information to uniquely identify a token on the origin
