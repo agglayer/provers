84d7483a0bb668c64c515477b25e5709386c484e
Marko Atanasievski <matanasievski@polygon.technology>
2025-03-25 20:45:34 +0100

    fix: tests

--- crates/proposer-service/src/tests/mod.rs
+++ crates/proposer-service/src/tests/mod.rs
@@ -1,20 +1,33 @@
 use std::sync::Arc;
 
-use alloy_primitives::{b256, FixedBytes, B256};
+use alloy_primitives::{FixedBytes};
 use proposer_client::{
     rpc::AggregationProofProposerRequest, FepProposerRequest, MockProposerClient,
 };
 use prover_alloy::MockProvider;
-use sp1_sdk::{Prover as _, SP1_CIRCUIT_VERSION};
+use sp1_sdk::{Prover as _, SP1PublicValues, SP1_CIRCUIT_VERSION};
 use tower::Service as _;
 
-use crate::{Error, ProposerService, VKeyHash};
+use crate::{Error, ProposerService};
 
 const ELF: &[u8] = include_bytes!("../../../prover-dummy-program/elf/riscv32im-succinct-zkvm-elf");
+
+fn generate_keys() -> (
+    sp1_sdk::SP1ProvingKey,
+    sp1_sdk::SP1VerifyingKey,
+    SP1PublicValues,
+) {
+    let stdin = sp1_sdk::SP1Stdin::new();
+    let client = sp1_sdk::ProverClient::builder().mock().build();
+    let (pk, vk) = client.setup(ELF);
+    let (public_values, _) = client.execute(&pk.elf, &stdin).run().unwrap();
+    (pk, vk, public_values)
+}
 
 #[tokio::test]
 async fn test_proposer_service() {
     let mut l1_rpc = MockProvider::new();
+
     l1_rpc
         .expect_get_block_number()
         .once()
@@ -33,33 +46,24 @@ async fn test_proposer_service() {
         },
     );
 
-    let stdin = sp1_sdk::SP1Stdin::new();
-
-    let (pk, public_values) = {
-        let client = sp1_sdk::ProverClient::builder().cpu().build();
-
-        let (pk, _vk) = client.setup(ELF);
-        let (public_values, _) = client.execute(&pk.elf, &stdin).run().unwrap();
-
-        (pk, public_values)
-    };
-
-    let vk_hash = {
+    let (pkey, vkey, public_values) = generate_keys();
+    {
         let mock_proof = sp1_sdk::SP1ProofWithPublicValues::create_mock_proof(
-            &pk,
+            &pkey,
             public_values,
             sp1_sdk::SP1ProofMode::Compressed,
             SP1_CIRCUIT_VERSION,
         );
-        let compressed = mock_proof.proof.try_as_compressed_ref().unwrap();
-        let vk_hash = VKeyHash::from_vkey(&compressed.vk);
 
         client
             .expect_wait_for_proof()
             .once()
             .return_once(move |_| Box::pin(async move { Ok(mock_proof) }));
 
-        vk_hash
+        client
+            .expect_verify_agg_proof()
+            .once()
+            .return_once(move |_, _, _| Ok(()));
     };
 
     let client = Arc::new(client);
@@ -67,7 +71,7 @@ async fn test_proposer_service() {
     let mut proposer_service = ProposerService {
         client,
         l1_rpc,
-        aggregation_vkey_hash: vk_hash,
+        aggregation_vkey_hash: vkey,
     };
 
     let request = FepProposerRequest {
@@ -81,76 +85,6 @@ async fn test_proposer_service() {
 }
 
 #[tokio::test]
-async fn test_vkey_hash_mismatch() {
-    let mut l1_rpc = MockProvider::new();
-    l1_rpc
-        .expect_get_block_number()
-        .once()
-        .returning(|_| Box::pin(async { Ok(10) }));
-
-    let mut client = MockProposerClient::new();
-    client.expect_request_agg_proof().once().returning(
-        |request: AggregationProofProposerRequest| {
-            Box::pin(async move {
-                Ok(proposer_client::rpc::AggregationProofProposerResponse {
-                    request_id: FixedBytes::new([0; 32]),
-                    start_block: request.start_block,
-                    end_block: request.max_block,
-                })
-            })
-        },
-    );
-
-    let stdin = sp1_sdk::SP1Stdin::new();
-
-    let (pk, public_values) = {
-        let client = sp1_sdk::ProverClient::builder().cpu().build();
-
-        let (pk, _vk) = client.setup(ELF);
-        let (public_values, _) = client.execute(&pk.elf, &stdin).run().unwrap();
-
-        (pk, public_values)
-    };
-
-    client.expect_wait_for_proof().once().return_once(move |_| {
-        Box::pin(async move {
-            Ok(sp1_sdk::SP1ProofWithPublicValues::create_mock_proof(
-                &pk,
-                public_values,
-                sp1_sdk::SP1ProofMode::Compressed,
-                SP1_CIRCUIT_VERSION,
-            ))
-        })
-    });
-
-    let vk_hash = VKeyHash::from_bytes(b256!(
-        "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbccccccccccccccccdddddddddddddddd"
-    ));
-
-    let client = Arc::new(client);
-    let l1_rpc = Arc::new(l1_rpc);
-    let mut proposer_service = ProposerService {
-        client,
-        l1_rpc,
-        aggregation_vkey_hash: vk_hash,
-    };
-
-    let request = FepProposerRequest {
-        start_block: 0,
-        max_block: 10,
-        l1_block_hash: Default::default(),
-    };
-
-    match proposer_service.call(request).await.unwrap_err() {
-        Error::AggregationVKeyMismatch { got, expected } => {
-            assert_ne!(got, expected);
-            assert_eq!(vk_hash, expected);
-        }
-        e => panic!("Unexpected error {e:?}"),
-    }
-}
-
-#[tokio::test]
 async fn unable_to_fetch_block_hash() {
     let mut l1_rpc = MockProvider::new();
     l1_rpc
@@ -160,12 +94,14 @@ async fn unable_to_fetch_block_hash() {
 
     let client = MockProposerClient::new();
 
+    let (_pkey, vkey, _public_values) = generate_keys();
+
     let client = Arc::new(client);
     let l1_rpc = Arc::new(l1_rpc);
     let mut proposer_service = ProposerService {
         client,
         l1_rpc,
-        aggregation_vkey_hash: B256::new([0x91; 32]).into(),
+        aggregation_vkey_hash: vkey,
     };
 
     let request = FepProposerRequest {
