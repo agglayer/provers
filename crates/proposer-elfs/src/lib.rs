09d9a8bbf86d79238076082d7617bdf6d68ab07d
L Kuklinek <lkuklinek@polygon.technology>
2025-04-04 17:18:44 +0100

    chore: make `VKEY_HASH` static to avoid wierd behaviour

--- crates/proposer-elfs/src/lib.rs
+++ crates/proposer-elfs/src/lib.rs
@@ -13,7 +13,7 @@ pub mod aggregation {
 
     pub const VKEY_HASH_U32: HashU32 = proposer_vkeys_raw::aggregation::VKEY_HASH;
 
-    pub const VKEY_HASH: VKeyHash = VKeyHash::from_hash_u32(VKEY_HASH_U32);
+    pub static VKEY_HASH: VKeyHash = VKeyHash::from_hash_u32(VKEY_HASH_U32);
 }
 
 pub mod range {
@@ -26,5 +26,5 @@ pub mod range {
 
     pub const VKEY_HASH_U32: HashU32 = proposer_vkeys_raw::range::VKEY_HASH;
 
-    pub const VKEY_HASH: VKeyHash = VKeyHash::from_hash_u32(VKEY_HASH_U32);
+    pub static VKEY_HASH: VKeyHash = VKeyHash::from_hash_u32(VKEY_HASH_U32);
 }
