a40d4c6fe83ae48b09c8a62c3eb972b5060bd5b4
L Kuklinek <lkuklinek@polygon.technology>
2025-04-04 18:23:10 +0100

    test: vkey consistency + static fix

--- crates/proposer-elfs/src/lib.rs
+++ crates/proposer-elfs/src/lib.rs
@@ -8,12 +8,12 @@ pub mod aggregation {
 
     use crate::{HashU32, LazyVerifyingKey, VKeyHash};
 
-    pub const VKEY: LazyVerifyingKey =
+    pub static VKEY: LazyVerifyingKey =
         LazyVerifyingKey::new_unchecked(proposer_vkeys_raw::aggregation::VKEY_BYTES);
 
     pub const VKEY_HASH_U32: HashU32 = proposer_vkeys_raw::aggregation::VKEY_HASH;
 
-    pub static VKEY_HASH: VKeyHash = VKeyHash::from_hash_u32(VKEY_HASH_U32);
+    pub const VKEY_HASH: VKeyHash = VKeyHash::from_hash_u32(VKEY_HASH_U32);
 }
 
 pub mod range {
@@ -21,10 +21,30 @@ pub mod range {
 
     use crate::{HashU32, LazyVerifyingKey, VKeyHash};
 
-    pub const VKEY: LazyVerifyingKey =
+    pub static VKEY: LazyVerifyingKey =
         LazyVerifyingKey::new_unchecked(proposer_vkeys_raw::range::VKEY_BYTES);
 
     pub const VKEY_HASH_U32: HashU32 = proposer_vkeys_raw::range::VKEY_HASH;
 
-    pub static VKEY_HASH: VKeyHash = VKeyHash::from_hash_u32(VKEY_HASH_U32);
+    pub const VKEY_HASH: VKeyHash = VKeyHash::from_hash_u32(VKEY_HASH_U32);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[rstest::rstest]
+    #[case::agg(aggregation::ELF, &aggregation::VKEY, aggregation::VKEY_HASH)]
+    #[case::range(range::ELF, &range::VKEY, range::VKEY_HASH)]
+    fn consistency(
+        #[case] elf: &[u8],
+        #[case] vkey: &LazyVerifyingKey,
+        #[case] vkey_hash: VKeyHash,
+    ) {
+        let prover = sp1_sdk::CpuProver::new();
+        let (_proving_key, computed_vkey) = sp1_sdk::Prover::setup(&prover, elf);
+
+        assert_eq!(VKeyHash::from_vkey(&computed_vkey), vkey_hash);
+        assert_eq!(VKeyHash::from_vkey(vkey.vkey()), vkey_hash);
+    }
 }
