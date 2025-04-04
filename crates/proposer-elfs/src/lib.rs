c4d287fe0b7e8fae377d454151cbbbcfbbf7e02b
L Kuklinek <lkuklinek@polygon.technology>
2025-04-04 16:41:16 +0100

    feat: lazy loaded vkeys

--- crates/proposer-elfs/src/lib.rs
+++ crates/proposer-elfs/src/lib.rs
@@ -1,9 +1,30 @@
+pub use aggkit_prover_types::{
+    vkey::LazyVerifyingKey,
+    vkey_hash::{HashU32, VKeyHash},
+};
+
 pub mod aggregation {
+    use crate::{HashU32, LazyVerifyingKey, VKeyHash};
+
     pub use op_succinct_elfs::AGG_ELF as ELF;
-    pub use proposer_vkeys_raw::aggregation::*;
+
+    pub const VKEY: LazyVerifyingKey =
+        LazyVerifyingKey::new_unchecked(proposer_vkeys_raw::aggregation::VKEY_BYTES);
+
+    pub const VKEY_HASH_U32: HashU32 = proposer_vkeys_raw::aggregation::VKEY_HASH;
+
+    pub const VKEY_HASH: VKeyHash = VKeyHash::from_hash_u32(VKEY_HASH_U32);
 }
 
 pub mod range {
+    use crate::{HashU32, LazyVerifyingKey, VKeyHash};
+
     pub use op_succinct_elfs::RANGE_ELF as ELF;
-    pub use proposer_vkeys_raw::range::*;
+
+    pub const VKEY: LazyVerifyingKey =
+        LazyVerifyingKey::new_unchecked(proposer_vkeys_raw::range::VKEY_BYTES);
+
+    pub const VKEY_HASH_U32: HashU32 = proposer_vkeys_raw::range::VKEY_HASH;
+
+    pub const VKEY_HASH: VKeyHash = VKeyHash::from_hash_u32(VKEY_HASH_U32);
 }
