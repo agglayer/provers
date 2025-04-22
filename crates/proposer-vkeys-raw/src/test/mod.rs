07346f7a78f05cb98f277379e7df1016e2e5541b
L Kuklinek <lkuklinek@polygon.technology>
2025-04-22 14:43:51 +0100

    test: use snapshots for proposer vkey

--- crates/proposer-vkeys-raw/src/test/mod.rs
+++ crates/proposer-vkeys-raw/src/test/mod.rs
@@ -1,20 +1,11 @@
-use alloy_primitives::{b256, B256};
+use alloy_primitives::B256;
 
 #[test]
-fn check_expected_values() {
-    let agg_vkey_hash: [u32; 8] = [
-        1638338388, 1256426946, 526664290, 1122453194, 1403846808, 601906877, 1176579776, 312933952,
-    ];
+fn aggregation_vkey_hash_snapshot() {
+    insta::assert_debug_snapshot!(crate::aggregation::VKEY_HASH);
+}
 
-    assert_eq!(
-        crate::aggregation::VKEY_HASH,
-        agg_vkey_hash,
-        "aggregation vkey hash mismatch"
-    );
-
-    assert_eq!(
-        B256::new(crate::range::VKEY_COMMITMENT),
-        b256!("0x35882a76205af8c12eaeea7551ff8dbc392dc2a95b0f7f31660a5468237d4434"),
-        "range vkey commitment mismatch"
-    )
+#[test]
+fn range_vkey_commitment_snapshot() {
+    insta::assert_debug_snapshot!(B256::new(crate::range::VKEY_COMMITMENT));
 }
