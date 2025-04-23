234a3388e35a928a5c1d3dbff1850381fa513d1b
L Kuklinek <lkuklinek@polygon.technology>
2025-04-23 15:15:39 +0100

    build: regenerate zkvm ELF

--- crates/proposer-vkeys-raw/src/bin/regenerate.rs
+++ crates/proposer-vkeys-raw/src/bin/regenerate.rs
@@ -1,12 +1,12 @@
 fn main() {
     prover_elf_utils::ElfInfo::writing_to_src("generated.rs")
         // Verification keys for aggregation proof
-        .module("aggregation", op_succinct_elfs::AGGREGATION_ELF)
+        .module("aggregation", op_succinct_elfs::AGG_ELF)
         .emit_vkey_bytes()
         .emit_vkey_hash()
         .finish()
         // Verification keys for range proof
-        .module("range", op_succinct_elfs::RANGE_ELF_EMBEDDED)
+        .module("range", op_succinct_elfs::RANGE_ELF)
         .emit_vkey_bytes()
         .emit_vkey_hash()
         .emit_vkey_commitment()
