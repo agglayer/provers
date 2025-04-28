f6b23af1295ea98ae174b86fc3062e5736c040db
leovct <l009.vincent@gmail.com>
2025-04-28 10:10:27 +0200

    chore: remove `trim_start_matches`

--- crates/aggkit-prover/src/main.rs
+++ crates/aggkit-prover/src/main.rs
@@ -39,12 +39,12 @@ fn main() -> anyhow::Result<()> {
             );
             let vkey = executor.get_vkey();
             let vkey_hex = hex::encode(vkey.hash_bytes());
-            println!("0x{}", vkey_hex.trim_start_matches("0x"));
+            println!("0x{vkey_hex}");
         }
 
         aggkit_prover::cli::Commands::VkeySelector => {
             let vkey_selector_hex = hex::encode(AGGCHAIN_VKEY_SELECTOR.to_be_bytes());
-            println!("0x{}", vkey_selector_hex.trim_start_matches("0x"));
+            println!("0x{vkey_selector_hex}");
         }
     }
 
