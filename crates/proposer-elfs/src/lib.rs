63fe2b3a7e384515316adaaa73f9f07267c7be43
Lukáš Kuklínek <lkuklinek@polygon.technology>
2025-05-09 11:13:07 +0100

    build: unify vkey crates (#193)

--- crates/proposer-elfs/src/lib.rs
+++ crates/proposer-elfs/src/lib.rs
@@ -3,48 +3,36 @@ pub use aggkit_prover_types::{
     vkey_hash::{HashU32, VKeyHash},
 };
 
+mod vkeys_raw {
+    include!(concat!(env!("OUT_DIR"), "/vkeys_raw.rs"));
+}
+
 pub mod aggregation {
     pub use op_succinct_elfs::AGG_ELF as ELF;
 
-    use crate::{HashU32, LazyVerifyingKey, VKeyHash};
+    use crate::{vkeys_raw, HashU32, LazyVerifyingKey, VKeyHash};
 
     pub static VKEY: LazyVerifyingKey =
-        LazyVerifyingKey::from_unparsed_bytes(proposer_vkeys_raw::aggregation::VKEY_BYTES);
+        LazyVerifyingKey::from_unparsed_bytes(vkeys_raw::aggregation::VKEY_BYTES);
 
-    pub const VKEY_HASH_U32: HashU32 = proposer_vkeys_raw::aggregation::VKEY_HASH;
+    pub const VKEY_HASH_U32: HashU32 = vkeys_raw::aggregation::VKEY_HASH;
 
     pub const VKEY_HASH: VKeyHash = VKeyHash::from_hash_u32(VKEY_HASH_U32);
 }
 
 pub mod range {
     pub use op_succinct_elfs::RANGE_ELF as ELF;
+    pub use vkeys_raw::range::VKEY_COMMITMENT;
 
-    use crate::{HashU32, LazyVerifyingKey, VKeyHash};
+    use crate::{vkeys_raw, HashU32, LazyVerifyingKey, VKeyHash};
 
     pub static VKEY: LazyVerifyingKey =
-        LazyVerifyingKey::from_unparsed_bytes(proposer_vkeys_raw::range::VKEY_BYTES);
+        LazyVerifyingKey::from_unparsed_bytes(vkeys_raw::range::VKEY_BYTES);
 
-    pub const VKEY_HASH_U32: HashU32 = proposer_vkeys_raw::range::VKEY_HASH;
+    pub const VKEY_HASH_U32: HashU32 = vkeys_raw::range::VKEY_HASH;
 
     pub const VKEY_HASH: VKeyHash = VKeyHash::from_hash_u32(VKEY_HASH_U32);
 }
 
 #[cfg(test)]
-mod test {
-    use super::*;
-
-    #[rstest::rstest]
-    #[case::agg(aggregation::ELF, &aggregation::VKEY, aggregation::VKEY_HASH)]
-    #[case::range(range::ELF, &range::VKEY, range::VKEY_HASH)]
-    fn consistency(
-        #[case] elf: &[u8],
-        #[case] vkey: &LazyVerifyingKey,
-        #[case] vkey_hash: VKeyHash,
-    ) {
-        let prover = sp1_sdk::CpuProver::new();
-        let (_proving_key, computed_vkey) = sp1_sdk::Prover::setup(&prover, elf);
-
-        assert_eq!(VKeyHash::from_vkey(&computed_vkey), vkey_hash);
-        assert_eq!(VKeyHash::from_vkey(vkey.vkey()), vkey_hash);
-    }
-}
+mod test;
